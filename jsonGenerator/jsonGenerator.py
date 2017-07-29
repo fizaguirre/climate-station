052044685a86784a363a71e4c942e791a0e6e052
fizaguirre <felipe.mizaguirre@gmail.com>
2017-07-29 17:04:21 -0300

    added queue

--- jsonGenerator/jsonGenerator.py
+++ jsonGenerator/jsonGenerator.py
@@ -7,50 +7,43 @@ import time
 from datetime import datetime
 from datetime import timedelta
 
+from azure.storage.queue import QueueService
+
 MESSAGE_STRING = 186
 
-def generate(n, filename):
-    daysBefore = n/24
+def generate(n):
 
+    queue_service = QueueService(account_name='izastorm',
+                    account_key='isUic1EEbXg8l53zUrl+o1Jmf8JPze/E8S5XQ3ActlrmpEmGqMSKdkSP/RTF4aFAdQmLeVy6DWT3pGJ1k/I2HA==')
     dateFormat = '%Y-%m-%d-%H-%M-%S'
-    #today = time.time()
-    lastPeriod = datetime.now() - timedelta(minutes=daysBefore)
-    print("Number of minutes %i" % daysBefore)
-    print("Start date %s" % lastPeriod.strftime(dateFormat))
 
-    currentHour = lastPeriod
-    with open(filename, 'w+') as f:
-        for i in range(n):
-            data ={}
-            datetimeValues = {}
-            datetimeValues['format'] = dateFormat
-            datetimeValues['source'] = 'RTC_DS1307'
-            #datetimeValues['value'] = time.strftime(dateFormat)
-            datetimeValues['value'] = currentHour.strftime(dateFormat)
-            currentHour = currentHour + timedelta(minutes=1)
+    while(True):
+        data ={}
+        datetimeValues = {}
+        datetimeValues['format'] = dateFormat
+        datetimeValues['source'] = 'RTC_DS1307'
+        datetimeValues['value'] = time.strftime(dateFormat)
 
-            sensorsValues = {}
-            sensorsValues['LDR'] = random.uniform(30.0,50.0)
-            sensorsValues['DHT22_AH'] = random.uniform(50.0, 70.0)
-            sensorsValues['BMP085_PRESSURE'] = random.uniform(90000.0, 110000.0)
-            sensorsValues['DHT22_TEMP'] = random.uniform(10.0, 40.0)
+        sensorsValues = {}
+        sensorsValues['LDR'] = random.uniform(30.0,50.0)
+        sensorsValues['DHT22_AH'] = random.uniform(50.0, 70.0)
+        sensorsValues['BMP085_PRESSURE'] = random.uniform(90000.0, 110000.0)
+        sensorsValues['DHT22_TEMP'] = random.uniform(10.0, 40.0)
 
-            data['sensors'] = sensorsValues
-            data['datetime'] = datetimeValues
+        data['sensors'] = sensorsValues
+        data['datetime'] = datetimeValues
 
-            f.write(json.dumps(data)+'\n')
-            #print json.dumps(data)
+        print json.dumps(data)
+        queue_service.put_message('stormtest', str(data).decode('utf-8'))
+        time.sleep(n)
 
 def main():
-    if len(sys.argv) == 3:
-        n = sys.argv[1]
-        fileName = sys.argv[2]
+    if len(sys.argv) == 2:
+        n = int(sys.argv[1])
 
-        lenghFile = float(n)*1024*1024*1024; # GB
-        nmessages = lenghFile / MESSAGE_STRING
-        print "Messages %i" %(nmessages)
+        print "Messages %s per second" %(n)
 
-        generate(int(nmessages), fileName)
+        generate(float(60.0/float(n)))
 
 
 if __name__ == "__main__":
