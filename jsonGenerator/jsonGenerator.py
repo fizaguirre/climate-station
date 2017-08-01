2aa3935faa9705fa43b643033300db711d4d949e
Felipe <felipe.izaguirre@e-core.com>
2017-08-01 20:10:51 -0300

    json generator converted to thread

--- jsonGenerator/jsonGenerator.py
+++ jsonGenerator/jsonGenerator.py
@@ -4,6 +4,7 @@ import json
 import sys
 import random
 import time
+import threading
 from datetime import datetime
 from datetime import timedelta
 
@@ -11,7 +12,8 @@ from azure.storage.queue import QueueService
 
 MESSAGE_STRING = 186
 
-def generate(n):
+def generate(n, id):
+    global TERMINATE
 
     queue_service = QueueService(account_name='izastorm',
                     account_key='isUic1EEbXg8l53zUrl+o1Jmf8JPze/E8S5XQ3ActlrmpEmGqMSKdkSP/RTF4aFAdQmLeVy6DWT3pGJ1k/I2HA==')
@@ -22,29 +24,62 @@ def generate(n):
         datetimeValues = {}
         datetimeValues['format'] = dateFormat
         datetimeValues['source'] = 'RTC_DS1307'
-        datetimeValues['stationID'] = random.randint(0,10)
+        #datetimeValues['stationID'] = random.randint(0,10)
+        datetimeValues['stationID'] = id
         datetimeValues['value'] = time.strftime(dateFormat)
 
         sensorsValues = {}
-        sensorsValues['LDR'] = random.uniform(30.0,50.0)
-        sensorsValues['DHT22_AH'] = random.uniform(50.0, 70.0)
+        sensorsValues['LDR'] = random.uniform(0.0,100.0)
+        sensorsValues['DHT22_AH'] = random.uniform(0.0, 100.0)
         sensorsValues['BMP085_PRESSURE'] = random.uniform(90000.0, 110000.0)
-        sensorsValues['DHT22_TEMP'] = random.uniform(10.0, 40.0)
+        sensorsValues['DHT22_TEMP'] = random.uniform(0.0, 45.0)
 
         data['sensors'] = sensorsValues
         data['datetime'] = datetimeValues
 
         print json.dumps(data).decode('utf-8')
         queue_service.put_message('stormtest', json.dumps(data).decode('utf-8'))
-        time.sleep(n)
+
+        try:
+            time.sleep(n)
+        except KeyboardInterrupt:
+            TERMINATE = False
+
+
+        if TERMINATE:
+            exit(0)
 
 def main():
-    if len(sys.argv) == 2:
-        n = int(sys.argv[1])
+    global TERMINATE
+    TERMINATE = False
+    if len(sys.argv) == 4:
+        interval = int(sys.argv[1])
+        nstations = int(sys.argv[2])
+        m = int(sys.argv[3])
 
-        print "Messages %s per second" %(n)
+        tsleep = float(60.0/float(interval))
 
-        generate(float(60.0/float(n)))
+        threads = []
+        for i in range(nstations * m, (nstations * m) + nstations):
+            t = threading.Thread(target=generate, args=(interval, tsleep))
+            #time.sleep(random.uniform(0.0,1.0))
+            threads.append(t)
+            t.start()
+
+        try:
+            while(True):
+                time.sleep(1)
+                #nothing
+        except KeyboardInterrupt:
+            print("Terminate")
+            TERMINATE = True
+
+        for t in threads:
+            t.join()
+
+        #print "Messages %s per second" %(n)
+
+        #generate(float(60.0/float(interval)))
 
 
 if __name__ == "__main__":
